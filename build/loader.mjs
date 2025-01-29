80e5cd643dcf872c5f2500f977a240b2fa6e520f
faceless49 <digital.zeus@mail.ru>
2025-01-29 19:06:26 +0300

    hw/1

--- build/loader.mjs
+++ build/loader.mjs
@@ -1,3 +1,26 @@
 import yaml from 'yaml';
 import path from 'node:path';
 import fs from 'node:fs/promises';
+import { fileURLToPath } from "node:url";
+
+export async function load(url, context, nextLoad) {
+  const ext = path.extname(url);
+
+  if (ext === '.yml' || ext === '.yaml') {
+    try {
+      const pathNameFile = fileURLToPath(url);
+
+      const contentFile = await fs.readFile(pathNameFile, 'utf8');
+
+      return {
+        format: 'json',
+        shortCircuit: true,
+        source: JSON.stringify(yaml.parse(contentFile))
+      };
+    } catch (err) {
+      throw err.message;
+    }
+  }
+
+  return nextLoad(url);
+}
